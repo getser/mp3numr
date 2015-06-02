f3451fe7922715ee6d4f00f3d0029421a98bd34c
Sergii Getmanchuk <getser1979@gmail.com>
2015-06-02 12:17:34 +0300

    Started ipmlementation pricess for mp3numr

---
+++ mp3numr.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+"""
+Randomly shuffles all .mp3 files in home folder, 
+then enumerates them by inserting the text
+from 001_ to XXX_ at the beginning of file name.
+If file has some digits at the begining - they will be removed.
+
+Run program inside folder that contains .mp3 files you want to
+shuffle and enumerate.
+"""

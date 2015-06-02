627e66f8cc6c3706ee6214d6ba952363600aaef8
Sergii Getmanchuk <getser1979@gmail.com>
2015-06-02 22:11:48 +0300

    Added 'get_new_name' func, changed filename format'_' => '. '

--- mp3numr.py
+++ mp3numr.py
@@ -14,7 +14,7 @@ shuffled and numbered.
 import os
 import random
 
-def crt_str_num(number, lenght):
+def create_string_num(number, lenght):
 	"""
 	Creates a string representation of given int() or str() 'number',
 	adding '0'-s in front of it to be given lenght.
@@ -24,8 +24,22 @@ def crt_str_num(number, lenght):
 		return str(number)
 	else:
 		next_number = '0'+ str(number)
-		return crt_str_num(next_number, lenght)
+		return create_string_num(next_number, lenght)
 #print crt_str_num(3, 5)
+
+def get_new_name(filename, prefix):
+	if filename[-4:] == '.mp3': name = filename[:-4]
+	else: name = filename
+
+	if len(name) > 0:
+		if name[0].isdigit() or name[0] in [' ', '_', '-', '.']:
+			name = name[1:]
+			return get_new_name(name, prefix)
+		else:
+			return prefix +'. '+ name + '.mp3'
+	else: 
+		return prefix + '.mp3'
+
 
 curr_path = os.path.abspath(os.curdir)
 text = 'If you want me to shuffle your .mp3 files \nin your %s \
@@ -43,7 +57,7 @@ num_digits = len(str(num_mp3_files))
 
 numbers_for_files = []
 for num in xrange(num_mp3_files):
-	numbers_for_files.append(crt_str_num(num + 1, num_digits))
+	numbers_for_files.append(create_string_num(num + 1, num_digits))
 #print numbers_for_files
 
 if to_shuffle == 'yes': 
@@ -52,5 +66,6 @@ if to_shuffle == 'yes':
 
 for i in xrange(len(mp3_files)):
 	old_name = mp3_files[i]
-	new_name = numbers_for_files[i]+'_'+mp3_files[i]
+#	new_name = numbers_for_files[i]+'_'+mp3_files[i][:-4] + '.mp3'
+	new_name = get_new_name(old_name, numbers_for_files[i])
 	os.rename(old_name, new_name)
